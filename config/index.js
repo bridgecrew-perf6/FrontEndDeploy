12cf709f9b0c8531185e42e90a8073f1b0da3fd2
srijan <bagdassrijan@hotmail.com>
2022-05-18 10:08:57 +0530

    final commit

--- config/index.js
+++ config/index.js
@@ -3,4 +3,5 @@ export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 //Frontend url
-export const APP_URL = "https://sikkimcoblood.vercel.app";
+export const APP_URL =
+  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
