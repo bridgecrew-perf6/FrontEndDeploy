5805997492ae1dda53f80882350734c89d1d7f43
srijan <bagdassrijan@hotmail.com>
2022-05-21 23:46:48 +0530

    Shovi2t commit

--- pages/about.js
+++ pages/about.js
@@ -8,26 +8,33 @@ export default function AboutPage() {
       <div className="divide">
         <h1>About</h1>
         <p>
-        This portal is built to fulfill the blood requirements and to register voluntary blood donors of the state,
-        so that a common and active database can be used to prevent loss of life due to blood shortage. 
-        We thank each and every one who has been part of this journey in any way, And also who will be in the coming days.
-        This portal was launched on 18th May, 2022 by Hon'ble Chief Minister Shri Prem Singh Tamang  accompanied by his spouse Smt. Krishna Rai in presence of Political Secretary Shri Jacob Khaling, Cabinet ministers, MLAs, Advisors, Chairpersons and other dignitaries on the occasion of 47th State Day Celebration and Sikkim Idol Grand finale at Mannan Kendra, Gangtok.
+          This portal is built to fulfill the blood requirements and to register
+          voluntary blood donors of the state, so that a common and active
+          database can be used to prevent loss of life due to blood shortage. We
+          thank each and every one who has been part of this journey in any way,
+          And also who will be in the coming days. This portal was launched on
+          18th May, 2022 by Hon'ble Chief Minister Shri Prem Singh Tamang
+          accompanied by his spouse Smt. Krishna Rai in presence of Political
+          Secretary Shri Jacob Khaling, Cabinet ministers, MLAs, Advisors,
+          Chairpersons and other dignitaries on the occasion of 47th State Day
+          Celebration and Sikkim Idol Grand finale at Mannan Kendra, Gangtok.
         </p>
         <p>&copy; Blood.Sikkim.Co 2022</p>
-        <p>Developed and designed by Srijan, Shovit,<br/> Logesh and Gaurav</p>
+        <p>
+          Developed and designed by Srijan, Shovit,
+          <br /> Logesh and Gaurav
+        </p>
         <p>
           <Link href="https://nedevelopers.in/">
-            <a>
-              Ne Developers &nbsp;
-            </a>
+            <a>Ne Developers &nbsp;</a>
           </Link>
-          <br/>
+          <br />
           Gangtok
-          <br/>
-              Contact: +91-9894178970
+          <br />
+          Contact: +91-9894178970
           <br />
         </p>
-        <br/>
+        <br />
         <h1>Support Partners</h1>
         {/* <p>
           On our journey to make the society lively and healthy, <br />
@@ -37,7 +44,7 @@ export default function AboutPage() {
         <div>
           <PhotoGrid evt={"/images/SupportPartners/bloodarmy.jpg"} />
           <PhotoGrid evt={"/images/SupportPartners/runners.jpg"} />
-          <PhotoGrid evt={"/images/SupportPartners/Twilio.jpg"} />
+          <PhotoGrid evt={"/images/SupportPartners/twilio.jpg"} />
           <PhotoGrid evt={"/images/SupportPartners/Friends.jpg"} />
           <PhotoGrid evt={"/images/SupportPartners/SC.jpg"} />
           <PhotoGrid evt={"/images/SupportPartners/vbdas.jpg"} />
